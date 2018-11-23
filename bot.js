6cd733def41847a8ca993d43ac681abb9978be18
Zino <41270843+zungf@users.noreply.github.com>
2018-11-23 05:33:43 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -75,7 +75,7 @@ client.on('ready', () => {
 
 
 client.on('guildMemberAdd', member => {
-const YossiF = member.guild.channels.get("506613002980818963");//ايدي الشات
+const YossiF = member.guild.channels.get("505479736236179498");//ايدي الشات
 if(!YossiF) return;
 if(YossiF) {
 YossiF.send(`**..Welcome :dove:   To :69: OnlyGames :leaves:  ..**`);          
@@ -84,7 +84,33 @@ YossiF.send(`**..Welcome :dove:   To :69: OnlyGames :leaves:  ..**`);
 
 
 
+const prefix = "!"
+client.on('message', message => {
+  if (!message.content.startsWith(prefix)) return;
+  var args = message.content.split(' ').slice(1);
+  var argresult = args.join(' ');
+  if (message.author.id !== '346454919328432128') return;
 
+if (message.content.startsWith(prefix + 'g')) {
+  client.user.setGame(argresult);
+    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
+} else 
+
+if (message.content.startsWith(prefix + 'w')) {
+client.user.setActivity(argresult, {type:'WATCHING'});
+    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
+} else 
+if (message.content.startsWith(prefix + 'l')) {
+client.user.setActivity(argresult, {type:'LISTENING'});
+    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
+} else 
+
+if (message.content.startsWith(prefix + 's')) {
+  client.user.setGame(argresult, "https://www.twitch.tv/Justin-Ly0001");
+    message.channel.sendMessage(`**:white_check_mark:  : ${argresult}**`)
+}
+
+});
 
 
 
